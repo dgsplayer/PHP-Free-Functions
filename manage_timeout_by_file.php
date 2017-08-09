9694a30f3f3a52d75a640d0029e63060362f6a66
dgsplayer <douglasmpcampos@gmail.com>
2017-08-09 12:26:44 -0300

    Update manage_timeout_by_file.php

--- manage_timeout_by_file.php
+++ manage_timeout_by_file.php
@@ -7,26 +7,26 @@
      */
 
 public function manage_timeout_by_file($file_name, $timeout){
-        @$lastTime = file_get_contents('/var/www/html/svx_prodesp/recursos/uploads/' . $file_name);
+        @$lastTime = file_get_contents('/var/www/html/project/recursos/uploads/' . $file_name);
 
         if((time() - @$lastTime) > $timeout){
-            file_put_contents('/var/www/html/svx_prodesp/recursos/uploads/' . $file_name, '');
+            file_put_contents('/var/www/html/project/recursos/uploads/' . $file_name, '');
         }
 
-        @$lastTime = file_get_contents('/var/www/html/svx_prodesp/recursos/uploads/' . $file_name);
+        @$lastTime = file_get_contents('/var/www/html/project/recursos/uploads/' . $file_name);
 
         if (empty($lastTime)){
             $lastTime = time();
-            file_put_contents('/var/www/html/svx_prodesp/recursos/uploads/' . $file_name, $lastTime);
+            file_put_contents('/var/www/html/project/recursos/uploads/' . $file_name, $lastTime);
             return true;
         }
 
-        file_put_contents('/var/www/html/svx_prodesp/recursos/uploads/' . $file_name, $lastTime);
+        file_put_contents('/var/www/html/project/recursos/uploads/' . $file_name, $lastTime);
 
         header('HTTP/1.1 500 Internal Server Error');
         exit(0);
 
-        die('Aguarde...' . ((int)time() - (int)@$lastTime) . ' de ' . $timeout . ' segundos');
+        die('Wait...' . ((int)time() - (int)@$lastTime) . ' on ' . $timeout . ' seconds');
 
     }
 
